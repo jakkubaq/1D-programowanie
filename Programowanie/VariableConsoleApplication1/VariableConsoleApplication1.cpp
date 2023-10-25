76afbe5b30f3dd3f253dd480bcda10141d68f487
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-25 07:34:57 +0200

    #

---
+++ Programowanie/VariableConsoleApplication1/VariableConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+// VariableConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";7
+}       
+
+
+
+
+/*
+Algorytm - zbiór instrukcij realizuj¹cy postawione przed nimk zadanie.
+Sposoby zapsiu:
+* w punktach
+* opis s³owny
+* jêzyk programowania lub pseudokod
+* zbiór rysunków
+* schemat blokowy
+* 
+* 
+* 
+*/
