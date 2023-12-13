b5aa81088ef5db153e4ece534afbb4b096642be5
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-13 09:50:52 +0100

    koniec

--- Programowanie/ConsoleApplicationInstructionIf/ConsoleApplicationInstructionIf.cpp
+++ Programowanie/ConsoleApplicationInstructionIf/ConsoleApplicationInstructionIf.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 
 /*
+Piorytety operatorów:
+* /
++ - 
+>< >= <= == !=
+&&
+||
+=
+
+operatory porowania:
 > wiekszosci
 < mniejszosci 
 >= wiekszy b¹d¿ rowny
 <= mniejszy b¹d¿ rowny
 == równe
 != rózne
+
+operatory logiczne:
+
+|| or/lub
+&& and/i
+!  not/nie
+
+A	B		A||B	A && B	  !A
+f	f		 f		  f		   t
+f	t		 t        f		   
+t	f		 t        f		   f
+t	t		 t        t
 */
 
 //Napisz program, który pobierze liczbê od uzytkownika i wyswietli czy jest ona dodatnia czy nie
@@ -34,11 +55,47 @@ void task1()
 	}
 	else
 	{
-		std::cout << "liczba jest ujemna\n";
+		std::cout << "liczba jest ujemna lub rowna zero\n";
+	}
+}
+
+//napisz program, który wczyta liczbe i sprawdzi czy jest ona z zakresu <1;10)
+void task2()
+{
+	int number;
+	std::cout << "Podaj liczbe\n";
+	std::cin >> number;
+	
+	if (number >= 1)
+	{
+		if (number < 10)
+		{
+			std::cout << "liczba nalezy do przedzia³u\n";
+		}
+		else
+		{
+			std::cout << "liczba nie nalezy do przedzia³u\n";
+		}
+	}
+	else
+	{
+		std::cout << "liczba nie jest w przedziale\n";
+	}
+
+	//wersja2
+	if (number >= 1 && number < 10)
+	{
+		std::cout << " liczba jest w zakresie\n";
+	}
+	else
+	{
+		std::cout << " liczba nie jest w zakresie\n";
 	}
 }
 
 int main()
 {
-	task1();
+	//task1();
+	task2();
 }
+
