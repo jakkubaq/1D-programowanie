ab1f7dda6d61e0b7728f855e7762b2da659f0a5a
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-02-02 13:48:55 +0100

    o

---
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -0,0 +1,44 @@
+﻿// LoopDoWhileConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+void task1()
+{
+	int number;
+	/*
+	std::cout << "podaj liczbe dodatnia\n";
+	std::cin >> number;
+
+	if (number < 0)
+	{
+		std::cout << "podaj liczbe dodatnia\n";
+		std::cin >> number;
+		if (number > 0)
+		{
+			std::cout << "podaj liczbe dodatnia\n";
+			std::cin >> number;
+			if (number > 0)
+			{
+					std::cout << "podaj liczbe dodatnia\n";
+					std::cin >> number;
+					if (number > 0)
+					{
+						//....
+					}
+			}
+		}
+		*/
+	do
+	{
+		std::cout << "podaj liczbe dodatnia\n";
+		std::cin >> number;
+	} while (number < 0);
+	std::cout << "podana liczbe " << number << "\n";
+}
+
+int main()
+{
+	task1();
+}
