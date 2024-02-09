f7b769017d1ced34e3bf2693536288441437fd66
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-02-09 10:00:00 +0100

    koniec lekcij

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -38,7 +38,39 @@ void task1()
 	std::cout << "podana liczbe " << number << "\n";
 }
 
+//napisz program wyswoetlający liczby całkowite z przedziału <0,x) (wartosc x podaje uzytkownik)
+void task3()
+{
+	int upperRange;
+	std::cout << "podaj górny zakres:\n";
+	std::cin >> upperRange;
+
+	
+	/*	std::cout << "0, ";
+	if (upperRange >= 1)
+	{
+		std::cout << "1, ";
+		if (upperRange >= 2)
+		{
+			std::cout << "2, ";
+			if(upperRange >=3)
+			{
+					//...
+			}
+		}
+	}
+	*/
+	int number = 0;
+	do
+	{
+		std::cout << number << ", ";
+		number++;
+	} while (upperRange >= number);
+
+}
+
 int main()
 {
-	task1();
+	//task1();
+	task3();
 }
