df4ee19553c948c0162406c40ca7fb2c377183c2
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-22 15:09:10 +0100

    k

---
+++ Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -0,0 +1,27 @@
+﻿// CharactersConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void task1()
+{
+    char characterFromUser;
+    std::cout << "Podaj jeden znak:\n";
+    std::cin >> characterFromUser;
+
+    std::cout << "Podano: " << characterFromUser << "\n";
+
+    characterFromUser = characterFromUser + 1;
+
+    std::cout << "Podano: " << characterFromUser << "\n";
+
+    characterFromUser = 97;
+    characterFromUser = 'a';
+    characterFromUser = 'a' + 1; // 'b'
+}
+
+int main()
+{
+	task1();
+}
+
