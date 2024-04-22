079e20991af11509879f559787d23b6232324a9d
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-04-22 09:36:30 +0200

    ll

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+// Napisz program, który wczyta np. 5 liczb 
+// a następnie wyświetli je w odwrotnej kolejności.
 void task1()
 {
 	/*
@@ -62,30 +64,101 @@ void task2()
 	std::cout << "Średnia to: " << avg << "\n";
 }
 
-//napisz program który uzupełni tablice liczbami losowymi
-//a nastepnie znajdzie minimum oraz max
+//Napisz program, który uzupełni tablicę liczbami losowymi
+// a następnie znajdzie minimum oraz maksimum.
 void task3()
 {
 	const unsigned int ARRAY_NUMBERS_SIZE = 10;
-	int number[ARRAY_NUMBERS_SIZE];
-	
+	int numbers[ARRAY_NUMBERS_SIZE];
+
 	srand(time(NULL));
-	std::cout << "liczby w kolekcij\n";
+	std::cout << "Liczby w kolekcji:\n";
 	for (int i = 0; i < ARRAY_NUMBERS_SIZE; i++)
 	{
-		numbers[i] = rand() % 11 - 2000;
+		numbers[i] = rand() % 100;
+		std::cout << numbers[i] << ", ";
+	}
+	std::cout << "\n\n";
 
+	int max = numbers[0];
+	for (int i = 1; i < ARRAY_NUMBERS_SIZE; i++)
+	{
+		if (numbers[i] > max)
+			max = numbers[i];
+	}
+	std::cout << "Największa liczba to " << max << "\n";
+
+	int min = numbers[0];
+	for (int i = 1; i < ARRAY_NUMBERS_SIZE; i++)
+	{
+		if (numbers[i] < min)
+			min = numbers[i];
+	}
+	std::cout << "Najmniejsza liczba to " << min << "\n";
+}
+
+//Napisz program, który wczyta numer dnia tygodania 
+// a następnie wyświetli nazwę tego dnia lub komunikat błedu.
+void task4()
+{
+	int dayNumberFromUser;
+	std::cout << "Podaj numer dnia tygodnia:\n";
+	std::cin >> dayNumberFromUser;
+
+	std::string nameOfDayInWeek[7] = { "Poniedziałek", "Wtorek" };
+	nameOfDayInWeek[0] = "Poniedziałek";
+	nameOfDayInWeek[1] = "Wtorek";
+	nameOfDayInWeek[2] = "Środa";
+	nameOfDayInWeek[3] = "Czwartek";
+	nameOfDayInWeek[4] = "Piątek";
+	nameOfDayInWeek[5] = "Sobota";
+	nameOfDayInWeek[6] = "Niedziela";
+
+	if (dayNumberFromUser >= 0 && dayNumberFromUser <= 6)
+	{
+		std::cout << "Nazwa dnia to " << nameOfDayInWeek[dayNumberFromUser] << "\n";
+	}
+	else
+	{
+		std::cout << "Nie ma takiego dnia\n";
 	}
 }
 
-//napisz program który wczyta numer dnia tygodnia
-//a nastepnie wyswietli nawe tego dnia lub komunikat błedu
-void task4()
+// Napisz program, który posortuje tablicę liczb sposobem przez wybór.
+void task5()
 {
-	const unsigned int ARRAY_NUMBERS_SIZE = 7;
+	const unsigned int ARRAY_NUMBERS_SIZE = 10;
 	int numbers[ARRAY_NUMBERS_SIZE];
 
-	std::cout << "podaj number dnia tygodnia\n"
+	srand(time(NULL));
+	std::cout << "Liczby w kolekcji:\n";
+	for (int i = 0; i < ARRAY_NUMBERS_SIZE; i++)
+	{
+		numbers[i] = rand() % 100;
+		std::cout << numbers[i] << ", ";
+	}
+	std::cout << "\n\n";
+
+	//algorytm sortujący
+	for (int i = 0; i <= ARRAY_NUMBERS_SIZE - 2; i++)
+	{
+		int minIndex = i;
+		for (int j = i + 1; j < ARRAY_NUMBERS_SIZE; j++)
+		{
+			if (numbers[j] < numbers[minIndex])
+				minIndex = j;
+		}
+		int tmp = numbers[i];
+		numbers[i] = numbers[minIndex];
+		numbers[minIndex] = tmp;
+	}
+
+	std::cout << "Po posortowaniu:\n";
+	for (int i = 0; i < ARRAY_NUMBERS_SIZE; i++)
+	{
+		std::cout << numbers[i] << ", ";
+	}
+	std::cout << "\n\n";
 }
 
 int main()
@@ -93,6 +166,7 @@ int main()
 	//task1();
 	//task2();
 	//task3();
-	task4();
+	//task4();
+	task5();
 }
 
